6fa019862ca244c7133f65d78fc728e82eee8a2e
Jonathan Mendes <T3499448>
2020-11-16 13:56:47 -0300

    [REFACTOR] - move code to helper classes to improve readability

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -59,20 +59,7 @@ extension RemoteAddAccountTests{
 
         return (sut, httpClientSpy)
     }
-    func checkMemoryLeak(for instance:AnyObject, file:StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, file: file, line: line)
-        }
-    }
-    
-    func makeInvalidData() -> Data {
-        return Data("invalid_data".utf8)
-    }
-    
-    func makeUrl() -> URL {
-        return URL(string: "http://any-url.com")!
-    }
-    
+
     func expect(_ sut:RemoteAddAccount, completeWith expectedResult:Result<AccountModel,DomainError>, when action: () -> Void,file:StaticString = #file, line: UInt = #line ){
         let exp = expectation(description: "waiting")
         sut.add(addAccountModel: makeAddAccountModel()) { receivedResult in
@@ -92,27 +79,6 @@ extension RemoteAddAccountTests{
     
     func makeAddAccountModel() -> AddAccountModel {
         return AddAccountModel(name: "any_name", email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_passwordConfirmation")
-            
     }
-    func makeAccountModel() -> AccountModel {
-        return AccountModel(id: "any_id", name: "any_name", email: "any_email@mail.com", password: "any_password")
-            
-    }
-    class HttpClientSpy: HttpPostClient{
-        var urls = [URL]()
-        var data: Data?
-        var completion :((Result<Data, HttpError>) -> Void)?
-            
-        func post(to url:URL, with data:Data?, completion: @escaping (Result<Data, HttpError>) -> Void)  {
-            self.urls.append(url)
-            self.data = data
-            self.completion = completion
-        }
-        func completeWithError(_ error: HttpError) {
-            completion?(.failure(error))
-        }
-        func completeWithData(_ data: Data) {
-            completion?(.success(data))
-        }
-    }
+      
 }
