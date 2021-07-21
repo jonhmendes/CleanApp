d67ccba00397cebc0e8b74c39a27c46754e81052
Jonathan Mendes <jmendes_brq>
2021-07-21 15:44:17 -0300

    [ADD] - ensure alamofireAdapter completes with error when no 200 http response is received

--- infraTests/AlamofireAdapterTest.swift
+++ infraTests/AlamofireAdapterTest.swift
@@ -2,19 +2,37 @@ import XCTest
 import Alamofire
 import Data
 
-class AlamofireAdapter {
+class AlamofireAdapter: HttpPostClient{
     private let session: SessionManager
     
     init(session: SessionManager = .default) {
         self.session = session
     }
     
-    func post(to url:URL, with data: Data?, completion: @escaping (Swift.Result<Data, HttpError>) -> Void) {
+    func post(to url:URL, with data: Data?, completion: @escaping (Swift.Result<Data?, HttpError>) -> Void) {
         session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).responseData
         { dataResponse in
+            guard let statusCode = dataResponse.response?.statusCode else { return
+                completion(.failure(.noConnectivity)) }
             switch dataResponse.result {
                 case .failure: completion(.failure(.noConnectivity))
-                case .success: break
+                case .success(let data):
+                    switch statusCode {
+                    case 204:
+                        completion(.success(nil))
+                    case 200...299:
+                        completion(.success(data))
+                    case 401:
+                        completion(.failure(.unauthorized))
+                    case 403:
+                        completion(.failure(.forbiden))
+                    case 400...499:
+                        completion(.failure(.badRequest))
+                    case 500...599:
+                        completion(.failure(.serverError))
+                    default:
+                        completion(.failure(.noConnectivity))
+                    }
             }
         }
     }
@@ -39,6 +57,36 @@ class AlamofireAdapterTest: XCTestCase {
     func test_post_should_complete_with_error_when_request_completes_with_error() {
         expectResult(.failure(.noConnectivity), when: (data: nil, response: nil, error: makeError()))
     }
+    
+    func test_post_should_complete_with_error_on_all_invalid_cases() {
+        expectResult(.failure(.noConnectivity), when: (data: makeValidData(), response: makeHttpResponse(), error: makeError()))
+        expectResult(.failure(.noConnectivity), when: (data: makeValidData(), response: nil, error: makeError()))
+        expectResult(.failure(.noConnectivity), when: (data: makeValidData(), response: nil, error: nil))
+        expectResult(.failure(.noConnectivity), when: (data: nil, response: makeHttpResponse(), error: makeError()))
+        //expectResult(.failure(.noConnectivity), when: (data: nil, response: makeHttpResponse(), error: nil))
+        expectResult(.failure(.noConnectivity), when: (data: nil, response: nil, error: nil))
+    }
+    
+    func test_post_should_complete_with_data_when_request_completes_with_200() {
+        expectResult(.success(makeValidData()), when: (data: makeValidData(), response: makeHttpResponse(), error: nil))
+    }
+    func test_post_should_complete_with_data_when_request_completes_with_204() {
+        expectResult(.success(nil), when: (data: nil, response: makeHttpResponse(statusCode: 204), error: nil))
+        expectResult(.success(nil), when: (data: makeEmptyData(), response: makeHttpResponse(statusCode: 204), error: nil))
+        expectResult(.success(nil), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 204), error: nil))
+    }
+    
+    func test_post_should_complete_with_error_when_request_completes_with_non_200() {
+        expectResult(.failure(.badRequest), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 400), error: nil))
+        expectResult(.failure(.unauthorized), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 401), error: nil))
+        expectResult(.failure(.forbiden), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 403), error: nil))
+        expectResult(.failure(.badRequest), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 450), error: nil))
+        expectResult(.failure(.badRequest), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 499), error: nil))
+        expectResult(.failure(.serverError), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 500), error: nil))
+        expectResult(.failure(.serverError), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 550), error: nil))
+        expectResult(.failure(.serverError), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 599), error: nil))
+
+    }
 }
 
 extension AlamofireAdapterTest {
@@ -61,7 +109,7 @@ extension AlamofireAdapterTest {
         action(request!)
     }
     
-    func expectResult(_ expectedResult: Swift.Result<Data, HttpError>, when stub: (data: Data?, response: HTTPURLResponse?,
+    func expectResult(_ expectedResult: Swift.Result<Data?, HttpError>, when stub: (data: Data?, response: HTTPURLResponse?,
         error: Error?), file: StaticString = #filePath, line: UInt = #line) {
         let sut = makeSut()
         UrlProtocolStub.simulate(data: stub.data, response: stub.response, error: stub.error)
