fb2790a4f25ba8750a3b193956723108ca8bbbdc
Jonathan Mendes <jmendes_brq>
2021-08-11 15:03:49 -0300

    [ADD] - UI Layer

---
+++ UITests/SignUpViewControllerTest.swift
@@ -0,0 +1,15 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Jonathan Carvalho Mendes on 11/08/21.
+//
+
+import XCTest
+
+class SignUpViewControllerTest: XCTestCase {
+    func test_() {
+        
+    }
+
+}
