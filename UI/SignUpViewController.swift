a57ccc2375ebd5fd812245d5cb83f34384ddf2b1
Jonathan Mendes <jmendes_brq>
2021-08-12 12:34:44 -0300

    [ADD] - ensure SignUpViewControle has a loading indicator and is a default

---
+++ UI/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  UI
+//
+//  Created by Jonathan Carvalho Mendes on 12/08/21.
+//
+
+import Foundation
+import UIKit
+
+final class SignUpViewController: UIViewController {
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
