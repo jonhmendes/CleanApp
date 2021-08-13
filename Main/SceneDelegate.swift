fa41b98c164f2301e8d0cb474a18942cab014d07
Jonathan Mendes <jmendes_brq>
2021-08-13 14:43:45 -0300

    [ADD] - Main target

---
+++ Main/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  Main
+//
+//  Created by Jonathan Carvalho Mendes on 12/08/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
